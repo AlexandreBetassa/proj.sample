56dd55504dd70b7b8556e4ce34dca358887a4dce
AlexandreBetassa <alexandre.betassa.souza@hotmail.com>
2023-02-08 14:43:02 -0300

    build project

---
+++ SampleGet/Controllers/SampleGet.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleGet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleGet : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
